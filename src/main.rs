a3424e4ecdd1568d7e4c2bd7f937e33bc331bf88
KeramKeram <rev@autograf.pl>
2024-12-17 21:43:09 +0100

    Init basic example of tui.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+use std::io;
+use tui::{backend::CrosstermBackend, Terminal};
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let stdout = io::stdout();
+    let backend = CrosstermBackend::new(stdout);
+    let mut terminal = Terminal::new(backend)?;
+    Ok(())
 }
