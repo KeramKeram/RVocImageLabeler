f3f988f52ffa53c65a322cf25f2d506494900aaa
KeramKeram <rev@autograf.pl>
2025-01-06 20:35:10 +0100

    Fix compilation warnings.

--- src/main.rs
+++ src/main.rs
@@ -6,28 +6,28 @@ fn main() {
 
     let mut flex = Flex::default().size_of_parent().column();
 
-    let mut label1 = Frame::default().with_label("Label 1:");
-    let mut input1 = Input::default();
-    flex.set_size(&label1, 30);
-    flex.set_size(&input1, 30);
+    let label1 = Frame::default().with_label("Label 1:");
+    let input1 = Input::default();
+    flex.fixed(&label1, 30);
+    flex.fixed(&input1, 30);
 
-    let mut label2 = Frame::default().with_label("Label 2:");
-    let mut input2 = Input::default();
-    flex.set_size(&label2, 30);
-    flex.set_size(&input2, 30);
+    let label2 = Frame::default().with_label("Label 2:");
+    let input2 = Input::default();
+    flex.fixed(&label2, 30);
+    flex.fixed(&input2, 30);
 
-    let mut label3 = Frame::default().with_label("Label 3:");
-    let mut input3 = Input::default();
-    flex.set_size(&label3, 30);
-    flex.set_size(&input3, 30);
+    let label3 = Frame::default().with_label("Label 3:");
+    let input3 = Input::default();
+    flex.fixed(&label3, 30);
+    flex.fixed(&input3, 30);
 
-    let mut label4 = Frame::default().with_label("Label 4:");
-    let mut input4 = Input::default();
-    flex.set_size(&label4, 30);
-    flex.set_size(&input4, 30);
+    let label4 = Frame::default().with_label("Label 4:");
+    let input4 = Input::default();
+    flex.fixed(&label4, 30);
+    flex.fixed(&input4, 30);
 
-    let mut ok_button = Button::default().with_label("OK");
-    flex.set_size(&ok_button, 30);
+    let ok_button = Button::default().with_label("OK");
+    flex.fixed(&ok_button, 30);
 
     flex.end();
     wind.end();
